a358b24ce3b6075cea69a35324b084195cf82ae3
nihaocun <nihaocun@163.com>
2018-08-13 13:52:46 +0800

    first

---
+++ app/src/main/java/top/runanjing/tmhandspeed/MainActivity.java
@@ -0,0 +1,14 @@
+package top.runanjing.tmhandspeed;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
